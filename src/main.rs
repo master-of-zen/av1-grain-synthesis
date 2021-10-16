7240536390442cb5f98ba1dc09ba7fffa04c0c56
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-10-16 19:07:24 +0300

    default args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{AppSettings, Parser};
+use clap::{AppSettings, Arg, Parser};
 use rand::distributions::{Distribution, Normal};
 use std::path::PathBuf;
 use v_frame::plane::Plane;
@@ -6,15 +6,17 @@ use v_frame::plane::Plane;
 #[derive(Parser, Debug)]
 #[clap(name = "av1-grain-synth", setting = AppSettings::DeriveDisplayOrder)]
 struct Args {
+    #[clap(short, long, default_value_t = 0.5)]
     mean: f64,
+
+    #[clap(short, long, default_value_t = 0.3)]
     std_dev: f64,
 }
 
 fn main() {
-    //let parsed = argparse::parse_args();
-    //println!("{:?}", parsed);
+    let args = Args::parse();
 
-    let normal = Normal::new(0.5, 0.3);
+    let normal = Normal::new(args.mean, args.std_dev);
 
     let vals = (0..4096)
         .into_iter()
